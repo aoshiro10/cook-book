d747ec6f94adb0fd7017d3ef620cd6ce0a875a66
aoshiro10 <albertooshiro90@gmail.com>
2019-07-25 11:01:46 -0400

    admin page

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Receipt(models.Model):
+    name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    author = models.CharField(max_length=200)
+    prep_time = models.IntegerField()
+    cook_time = models.IntegerField()
+    ingredients = models.TextField()
+    directions = models.TextField()
+    
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template.response import TemplateResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return TemplateResponse(request, 'app/list.html')
