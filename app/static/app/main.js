cb27fb3e5c26da4fa0a833f146bab138b484d541
aoshiro10 <albertooshiro90@gmail.com>
2019-07-25 15:12:54 -0400

    filter

--- app/static/app/main.js
+++ app/static/app/main.js
@@ -1,3 +1,12 @@
+$(document).ready(() => {
+    $('.search_bar').keypress((event) => {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode == '13') {
+            filter();
+        }
+    });
+});
+
 function filter() {
     const filter_text = $('.search_bar').val();
     $.ajax({
@@ -7,8 +16,8 @@ function filter() {
             'filter_text': filter_text
         },
         success: (data) => {
-            $('div_list').html(data.recipes_html);
-            alert("Success");
+            $('.div_list').html(data.recipes_html);
+            //alert(data.recipes_html);
         },
         error: (xhr, status, error) => {
             alert("Failure");
